46b383af6f909e157f050dc1b265ed255228f7f6
gpudemystified <contact@gpudemystified.com>
2025-10-05 15:18:32 +0300

    added cuda suggestions for code editor

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -6,6 +6,246 @@ require(['vs/editor/editor.main'], function() {
     // Register CUDA language
     monaco.languages.register({ id: 'cuda' });
     
+    // Add CUDA completion items
+    monaco.languages.registerCompletionItemProvider('cuda', {
+        provideCompletionItems: () => {
+            const suggestions = [
+                {
+                    label: '__global__',
+                    kind: monaco.languages.CompletionItemKind.Keyword,
+                    insertText: '__global__',
+                    detail: 'CUDA global function decorator',
+                    documentation: 'Declares a function that runs on the GPU and is callable from the CPU'
+                },
+                {
+                    label: 'threadIdx',
+                    kind: monaco.languages.CompletionItemKind.Variable,
+                    insertText: 'threadIdx.${1:x}',
+                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                    detail: 'Thread index within a block',
+                    documentation: 'Access the current thread index (x, y, or z component)'
+                },
+                {
+                    label: 'blockIdx',
+                    kind: monaco.languages.CompletionItemKind.Variable,
+                    insertText: 'blockIdx.${1:x}',
+                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                    detail: 'Block index within the grid',
+                    documentation: 'Access the current block index (x, y, or z component)'
+                },
+                {
+                    label: 'cudaMalloc',
+                    kind: monaco.languages.CompletionItemKind.Function,
+                    insertText: 'cudaMalloc((void**)&${1:ptr}, ${2:size});',
+                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                    detail: 'Allocate memory on the GPU',
+                    documentation: 'Allocates size bytes of linear memory on the device'
+                },
+                {
+                    label: '__shared__',
+                    kind: monaco.languages.CompletionItemKind.Keyword,
+                    insertText: '__shared__ ${1:type} ${2:variable}[${3:size}];',
+                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                    detail: 'CUDA shared memory decorator',
+                    documentation: 'Declares a variable in shared memory, accessible by all threads within the same block'
+                },
+                {
+                    label: 'cudaMemcpy',
+                    kind: monaco.languages.CompletionItemKind.Function,
+                    insertText: 'cudaMemcpy(${1:dst}, ${2:src}, ${3:size}, ${4:cudaMemcpyHostToDevice});',
+                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                    detail: 'Copy memory between host and device',
+                    documentation: {
+                        value: [
+                            '```cuda',
+                            'cudaError_t cudaMemcpy(void* dst, const void* src, size_t size, cudaMemcpyKind kind)',
+                            '```',
+                            'Copies data between host and device memory.',
+                            '\n\nMemcpy kinds:',
+                            '- cudaMemcpyHostToDevice: Host -> Device',
+                            '- cudaMemcpyDeviceToHost: Device -> Host',
+                            '- cudaMemcpyDeviceToDevice: Device -> Device'
+                        ].join('\n')
+                    }
+                },
+                {
+                    label: 'blockDim',
+                    kind: monaco.languages.CompletionItemKind.Variable,
+                    insertText: 'blockDim.${1:x}',
+                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                    detail: 'Block dimensions',
+                    documentation: 'Number of threads in each dimension of a block'
+                },
+                {
+                    label: 'gridDim',
+                    kind: monaco.languages.CompletionItemKind.Variable,
+                    insertText: 'gridDim.${1:x}',
+                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                    detail: 'Grid dimensions',
+                    documentation: 'Number of blocks in each dimension of the grid'
+                },
+                {
+                    label: '__syncthreads',
+                    kind: monaco.languages.CompletionItemKind.Function,
+                    insertText: '__syncthreads();',
+                    detail: 'Synchronize threads in a block',
+                    documentation: 'Creates a barrier where all threads in a block must wait before proceeding'
+                },
+                {
+                    label: '__syncwarp',
+                    kind: monaco.languages.CompletionItemKind.Function,
+                    insertText: '__syncwarp();',
+                    detail: 'Synchronize threads in a warp',
+                    documentation: 'Creates a barrier where all threads in a warp must wait before proceeding'
+                },
+                {
+                    label: 'atomicAdd',
+                    kind: monaco.languages.CompletionItemKind.Function,
+                    insertText: 'atomicAdd(${1:address}, ${2:value})',
+                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                    detail: 'Atomic addition',
+                    documentation: 'Atomically adds value to the variable at address'
+                },
+                {
+                    label: 'atomicSub',
+                    kind: monaco.languages.CompletionItemKind.Function,
+                    insertText: 'atomicSub(${1:address}, ${2:value})',
+                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                    detail: 'Atomic subtraction',
+                    documentation: 'Atomically subtracts value from the variable at address'
+                },
+                {
+                    label: 'atomicExch',
+                    kind: monaco.languages.CompletionItemKind.Function,
+                    insertText: 'atomicExch(${1:address}, ${2:value})',
+                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                    detail: 'Atomic exchange',
+                    documentation: 'Atomically exchanges value with the value at address'
+                },
+                {
+                    label: 'atomicMin',
+                    kind: monaco.languages.CompletionItemKind.Function,
+                    insertText: 'atomicMin(${1:address}, ${2:value})',
+                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                    detail: 'Atomic minimum',
+                    documentation: 'Atomically computes minimum of value and the value at address'
+                },
+                {
+                    label: 'atomicAnd',
+                    kind: monaco.languages.CompletionItemKind.Function,
+                    insertText: 'atomicAnd(${1:address}, ${2:value})',
+                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                    detail: 'Atomic AND',
+                    documentation: 'Atomically performs bitwise AND of value and the value at address'
+                },
+                {
+                    label: 'atomicCAS',
+                    kind: monaco.languages.CompletionItemKind.Function,
+                    insertText: 'atomicCAS(${1:address}, ${2:compare}, ${3:value})',
+                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                    detail: 'Atomic Compare-and-Swap',
+                    documentation: 'Atomically performs compare-and-swap operation'
+                },
+                {
+                    label: 'cudaFree',
+                    kind: monaco.languages.CompletionItemKind.Function,
+                    insertText: 'cudaFree(${1:ptr});',
+                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                    detail: 'Free device memory',
+                    documentation: 'Frees memory previously allocated by cudaMalloc'
+                },
+                {
+                    label: 'cudaMemset',
+                    kind: monaco.languages.CompletionItemKind.Function,
+                    insertText: 'cudaMemset(${1:ptr}, ${2:value}, ${3:size});',
+                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                    detail: 'Set device memory',
+                    documentation: 'Sets device memory to a value'
+                },
+                {
+                    label: 'cudaMallocManaged',
+                    kind: monaco.languages.CompletionItemKind.Function,
+                    insertText: 'cudaMallocManaged((void**)&${1:ptr}, ${2:size});',
+                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                    detail: 'Allocate managed memory',
+                    documentation: 'Allocates managed memory accessible by both CPU and GPU'
+                },
+                {
+                    label: 'cudaStreamCreate',
+                    kind: monaco.languages.CompletionItemKind.Function,
+                    insertText: 'cudaStreamCreate(&${1:stream});',
+                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                    detail: 'Create CUDA stream',
+                    documentation: 'Creates a new asynchronous stream'
+                },
+                {
+                    label: 'cudaStreamDestroy',
+                    kind: monaco.languages.CompletionItemKind.Function,
+                    insertText: 'cudaStreamDestroy(${1:stream});',
+                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                    detail: 'Destroy CUDA stream',
+                    documentation: 'Destroys and cleans up an asynchronous stream'
+                },
+                {
+                    label: 'cudaStreamSynchronize',
+                    kind: monaco.languages.CompletionItemKind.Function,
+                    insertText: 'cudaStreamSynchronize(${1:stream});',
+                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                    detail: 'Synchronize CUDA stream',
+                    documentation: 'Waits for all operations in the stream to complete'
+                },
+                {
+                    label: 'cudaEventCreate',
+                    kind: monaco.languages.CompletionItemKind.Function,
+                    insertText: 'cudaEventCreate(&${1:event});',
+                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                    detail: 'Create CUDA event',
+                    documentation: 'Creates a new event'
+                },
+                {
+                    label: 'cudaEventRecord',
+                    kind: monaco.languages.CompletionItemKind.Function,
+                    insertText: 'cudaEventRecord(${1:event}, ${2:stream});',
+                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                    detail: 'Record CUDA event',
+                    documentation: 'Records an event in a stream'
+                },
+                {
+                    label: 'cudaEventSynchronize',
+                    kind: monaco.languages.CompletionItemKind.Function,
+                    insertText: 'cudaEventSynchronize(${1:event});',
+                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                    detail: 'Synchronize CUDA event',
+                    documentation: 'Waits for an event to complete'
+                },
+                {
+                    label: 'cudaEventElapsedTime',
+                    kind: monaco.languages.CompletionItemKind.Function,
+                    insertText: 'cudaEventElapsedTime(&${1:ms}, ${2:start}, ${3:stop});',
+                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                    detail: 'Calculate elapsed time',
+                    documentation: 'Computes the elapsed time between two events in milliseconds'
+                },
+                {
+                    label: 'cudaGetDevice',
+                    kind: monaco.languages.CompletionItemKind.Function,
+                    insertText: 'cudaGetDevice(&${1:device});',
+                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                    detail: 'Get current device',
+                    documentation: 'Gets the current CUDA device'
+                },
+                {
+                    label: 'cudaDeviceSynchronize',
+                    kind: monaco.languages.CompletionItemKind.Function,
+                    insertText: 'cudaDeviceSynchronize();',
+                    detail: 'Synchronize device',
+                    documentation: 'Waits for all operations on the device to complete'
+                }
+            ];
+            return { suggestions };
+        }
+    });
+
     // Configure CUDA syntax highlighting
     monaco.languages.setMonarchTokensProvider('cuda', {
         keywords: [
